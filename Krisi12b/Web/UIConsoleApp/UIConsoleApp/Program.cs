29a54f9729ce9fc8fe4c6cef3bbf311b22a4f85a
Kristian Vladimirov <kris20003@abv.bg>
2021-10-22 00:11:50 +0300

    new

---
+++ Krisi12b/Web/UIConsoleApp/UIConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using System;
+
+namespace UIConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductContext db = new ProductContext();
+            db.Database.EnsureCreated();
+            Display display = new Display();
+        }
+    }
+}
